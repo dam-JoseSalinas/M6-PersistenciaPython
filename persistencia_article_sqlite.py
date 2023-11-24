3821f9310f618db214a00bd63a4e21d245f31494
JoseSalinas <2023_jose.salinas@iticbcn.cat>
2023-11-24 12:27:23 +0100

    persistencia_article

--- persistencia_article_sqlite.py
+++ persistencia_article_sqlite.py
@@ -9,9 +9,9 @@ import sqlite3
 import logging
 
 class Persistencia_article_sqlite(IPersistencia_article):
-    def __init__(self, sqlite_namefile):
-        self._credencials = credencials
-        self._conn = sqilte3.connect()
+    def __init__(self, sqlite_path):
+        self._conn = sqlite3.connect(sqlite_path)
+        
         if not self.check_table():
             logging.debug("[Persistencia article] Creant taula d'articles.")
             self.create_table()
@@ -19,20 +19,22 @@ class Persistencia_article_sqlite(IPersistencia_article):
      
     def check_table(self):
         try:
-            cursor = self._conn.cursor(buffered=True)
+            #buffered = lambda cur: (cur, cur.fetchone())
+            #cursor, buffer = self._conn.cursor().buffered()
+            cursor = self._conn.cursor()
             cursor.execute("SELECT * FROM articles;")
-            cursor.reset()
-        except mysql.connector.errors.ProgrammingError:
+            #cursor = reset = None
+        except sqlite3.OperationalError:
             return False
         return True
+
 
     def create_table(self):
         cursor = self._conn.cursor()
         cursor.execute("""
                     Create table if not exists articles(
-                      id int not null auto_increment,
-                      nom varchar(255) unique,
-                      primary key (id)
+                      id integer not null primary key autoincrement,
+                      nom varchar(255) unique
                     )
                       """)
         self._conn.commit()
@@ -42,34 +44,30 @@ class Persistencia_article_sqlite(IPersistencia_article):
     
     def desa(self, article: Article) -> Article:
         cursor = self._conn.cursor()
-        query = "insert into articles (nom) values(%s);"
+        query = "insert into articles (nom) values(?);"
         parameters = (article.nom,)
         try:
-            cursor.execute(query,parameters)
+            cursor.execute(query, parameters)
             logging.info(f"[Persistencia] Nou article amb id = {cursor.lastrowid}.")
-            self._conn.commit() 
-        except mysql.connector.errors.IntegrityError:
+            self._conn.commit()
+        except sqlite3.IntegrityError:
             logging.info("[Persistencia] Intent de tornar a escriure un article existent.")
         return self.llegeix(article.nom)
         
     def llegeix(self, nom: str) -> Article:
-        cursor = self._conn.cursor(buffered=True)
-        query = "select id, nom from articles where nom = %s;"
+        cursor = self._conn.cursor()
+        query = "select id, nom from articles where nom = ?;"
         parameters = (nom,)
         cursor.execute(query, parameters)
         registre = cursor.fetchone()
-        cursor.reset()
+        #cursor = reset = None
         if not registre is None:
-            return Article(registre[1], self,registre[0])
+            return Article(nom=registre[1], persistencia=self, id=registre[0])
         return None
 
 if __name__ == "__main__":
     logging.basicConfig(filename='llista_compra.log', encoding='utf-8', level=logging.DEBUG)
-    p = Persistencia_article_mysql({
-        "host": "localhost",
-        "user": "dam_app",
-        "password": "1234",
-        "database": "dam_m6"})
+    p = Persistencia_article_sqlite("articles.db")
     a = Article("patates", p)
     a = a.persistencia.desa(a)
     print(a.persistencia.llegeix(a.nom))
