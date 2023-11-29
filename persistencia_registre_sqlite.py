e42aac9eaf49811b359c302e17a27655b2733a95
JoseSalinas <2023_jose.salinas@iticbcn.cat>
2023-11-29 17:18:02 +0100

    persistencia categoria

--- persistencia_registre_sqlite.py
+++ persistencia_registre_sqlite.py
@@ -7,28 +7,23 @@ Categoria modela una col·lecció d'articles rel·lacionats entre si.
 from ipersistencia_registre import IPersistencia_registre
 from registre import Registre
 from article import Article
-import mysql.connector
+import sqlite3
 import logging
 
 class Persistencia_registre(IPersistencia_registre):
-    def __init__(self, credencials):
-        self._credencials = credencials
-        self._conn = mysql.connector.connect(
-                host=credencials["host"],
-                user=credencials["user"],
-                password=credencials["password"],
-                database=credencials["database"]
-                )
+    def __init__(self, sqlite_path):
+        self.sqlite_path = sqlite_path
+        self._conn = sqlite3.connect(sqlite_path)
         if not self.check_table():
             self.create_table()
 
      
     def check_table(self):
         try:
-            cursor = self._conn.cursor(buffered=True)
+            cursor = self._conn.cursor()
             cursor.execute("SELECT * FROM registres;")
-            cursor.reset()
-        except mysql.connector.errors.ProgrammingError:
+            #cursor.reset()
+        except sqlite3.OperationalError:
             return False
         return True
 
@@ -36,20 +31,22 @@ class Persistencia_registre(IPersistencia_registre):
         cursor = self._conn.cursor()
         cursor.execute("""
                     Create table if not exists registres(
-                      id int not null auto_increment,
-                      article int unique references articles(id),
-                      quantitat int not null,
-                      primary key (id)
+                      id integer not null primary key autoincrement,
+                      article integer unique,
+                      quantitat integer not null,
+                      foreign key(article) references articles(id)
                     )
                       """)
         self._conn.commit()
 
     def tots(self):
-        cursor = self._conn.cursor(buffered=True)
-        query = "select registres.id, articles.id, articles.nom, registres.quantitat from registres, articles where registres.article = articles.id;"
+        cursor = self._conn.cursor() #buffered
+        query = """select registres.id, articles.id, articles.nom, registres.quantitat 
+                from registres, articles 
+                where registres.article = articles.id;"""
         cursor.execute(query)
         registres = cursor.fetchall()
-        cursor.reset()
+        #reset
         resultat = []
         for registre in registres:
             resultat.append(Registre(Article(registre[2], registre[1]), registre[3], registre[0])) 
@@ -57,40 +54,32 @@ class Persistencia_registre(IPersistencia_registre):
 
     def desa(self, registre: Registre) -> Registre:
         cursor = self._conn.cursor()
-        query = "insert into registres (article, quantitat) values(%s, %s);"
+        query = "insert into registres (article, quantitat) values(?, ?);"
         parameters = (registre.article.id, registre.quantitat)
         try:
             cursor.execute(query,parameters)
             logging.info(f"[Persistencia] Nou registre amb id = {cursor.lastrowid}.")
             self._conn.commit() 
-        except mysql.connector.errors.IntegrityError:
+        except sqlite3.IntegrityError:
             logging.info("[Persistencia] Integrity error escrivint registre.")
         return self.llegeix(registre.article.nom)
         
     def llegeix(self, nom: str) -> Registre:
-        cursor = self._conn.cursor(buffered=True)
-        query = "select registres.id, articles.id, articles.nom, registres.quantitat from registres, articles where registres.article = articles.id and articles.nom = %s;"
+        cursor = self._conn.cursor() #bufered
+        query = "select registres.id, articles.id, articles.nom, registres.quantitat from registres, articles where registres.article = articles.id and articles.nom = ?;"
         parameters = (nom,)
         cursor.execute(query, parameters)
         registre = cursor.fetchone()
-        cursor.reset()
+        #cursor.reset()
         if not registre is None:
             return Registre(Article(registre[2], registre[1]), registre[3], registre[0])
         return None
 
 if __name__ == "__main__":
-    from persistencia_article_mysql import Persistencia_article_mysql
+    from persistencia_article_sqlite import Persistencia_article_sqlite
     logging.basicConfig(filename='llista_compra.log', encoding='utf-8', level=logging.DEBUG)
-    pa = Persistencia_article_mysql({
-        "host": "localhost",
-        "user": "dam_app",
-        "password": "1234",
-        "database": "dam_m6"})
-    pr = Persistencia_registre({
-        "host": "localhost",
-        "user": "dam_app",
-        "password": "1234",
-        "database": "dam_m6"})
+    pa = Persistencia_article_sqlite("registres.db")
+    pr = Persistencia_registre("registres.db")
     a = Article("Arengades", pa)
     pa.desa(a)
     a = pa.llegeix('Arengades')
